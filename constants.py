98efd7b7c822d7e60d944e418d1b6bf37e31225f
ahmetkca <74574469+ahmetkca@users.noreply.github.com>
2021-03-28 15:32:47 -0400

    Added appropriate comments

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
-MAX_DEGREE = 180
-MIN_DEGREE = 0
-STEP_DEGREE = 22.5
-NUMBER_IMAGES = 10
-NUMBER_OF_DIGITS = 10
-NUM_PROJECTIONS = int(MAX_DEGREE / STEP_DEGREE)
-IMAGE_SIZE = 28
+MAX_DEGREE = 180 # Maximum degree you want to rotate the image
+MIN_DEGREE = 0 # starting degree it is 0 degree by default
+STEP_DEGREE = 22.5 # how many degree you want to increas after each iteration
+NUMBER_IMAGES = 10 # Number of images you have in each seperate digit folder
+NUMBER_OF_DIGITS = 10 # How many digit folder you have
+NUM_PROJECTIONS = int(MAX_DEGREE / STEP_DEGREE) # Calculates number of projections based on maximum degree and step degree
+IMAGE_SIZE = 28 # Size of the image so in this case 28 pixel by 28 pixel
