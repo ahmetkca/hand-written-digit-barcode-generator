78c5a94ea8ba71ec3007060436a539027b5097a8
ahmetkca <ahmet.karapinar@ontariotechu.net>
2021-03-28 20:24:05 -0400

    Added scrollable matplotlib plot where you can scroll through each results and also modifed the requirements.txt file

--- search_similar_image.py
+++ search_similar_image.py
@@ -221,7 +221,7 @@ class SearchSimilar:
             digitFolder = int(input("enter a digit (0 - 9): "))
         
     def showAllResults(self):
-        fig = plt.figure(figsize=(10, 7))
+        fig = plt.figure(figsize=(16,100), dpi=100)
         rows, cols = constants.NUMBER_OF_DIGITS*constants.NUMBER_IMAGES, 2
         for currDigit in range(constants.NUMBER_OF_DIGITS): # loop through 0 to NUMBER_OF_DIGITS-1  
             directory = r'./MNIST_DS/{}'.format(currDigit) # digit folder path
@@ -239,19 +239,53 @@ class SearchSimilar:
                 fig.add_subplot(rows, cols, sii)
                 plt.imshow(selectedImage)
                 plt.axis("off")
-                plt.title(selectedImagePath)
+                plt.title(selectedImagePath, fontsize=9, y=0.90)
                 fig.add_subplot(rows, cols, sii+1)
                 plt.imshow(resultImage)
                 plt.axis("off")
-                plt.title(resultImagePath)
+                plt.title(resultImagePath, fontsize=9, y=0.90)
             
-        plt.show()
+        return fig
+
+import matplotlib
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+from PyQt5 import QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+
+class ScrollableWindow(QtWidgets.QMainWindow):
+    def __init__(self, fig):
+        self.qapp = QtWidgets.QApplication([])
+
+        QtWidgets.QMainWindow.__init__(self)
+        self.widget = QtWidgets.QWidget()
+        self.setCentralWidget(self.widget)
+        self.widget.setLayout(QtWidgets.QVBoxLayout())
+        self.widget.layout().setContentsMargins(0,0,0,0)
+        self.widget.layout().setSpacing(0)
+
+        self.fig = fig
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.draw()
+        self.scroll = QtWidgets.QScrollArea(self.widget)
+        self.scroll.setWidget(self.canvas)
+
+        self.nav = NavigationToolbar(self.canvas, self.widget)
+        self.widget.layout().addWidget(self.nav)
+        self.widget.layout().addWidget(self.scroll)
+
+        self.show()
+        exit(self.qapp.exec_())
 
 
 
 if __name__ == "__main__":
     si = SearchSimilar()
-    si.showAllResults()
+    fig = si.showAllResults()
+    a = ScrollableWindow(fig)
     exit()
     cahr = CalculateAccuracyHitRatio(barcodes, imageLocations)
     print(cahr.calculateAccuracy())
